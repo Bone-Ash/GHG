1d398386c8a77c2858790a46105f1f8ba02dc334
GH <BoneAsh@iCloud.com>
2024-09-13 11:04:28 +0800

    Fix: Delete moyaRequest(Old)

--- Sources/GHG/Extension/MoyaProvider.swift
+++ Sources/GHG/Extension/MoyaProvider.swift
@@ -11,41 +11,6 @@ import SwiftyJSON
 
 @MainActor
 public extension MoyaProvider where Target == MultiTarget {
-    /// 带超时处理和 Toast 显示的请求方法
-    /// - Parameters:
-    ///   - target: 请求接口
-    ///   - showLoading: 是否显示 Loading
-    ///   - completion: 处理响应的闭包
-    ///
-    /// - Author: GH
-    func moyaRequest(_ target: TargetType, showLoading: Bool = false, completion: @escaping (Result<Response, MoyaError>) -> Void) {
-        if showLoading { ToastManager.shared.loadingToast() }
-        
-        self.request(MultiTarget(target)) { [weak self] result in
-            if showLoading { ToastManager.shared.hideLoadingToast() }
-            
-            guard let self = self else { return }
-            
-            switch result {
-            case .success(let response):
-                completion(.success(response))
-#if DEBUG
-                // TODO: 后续可删除
-                let json = JSON(response.data)
-                print(json)
-#endif
-            case .failure(let error):
-                if let underlyingError = (error as NSError).userInfo[NSUnderlyingErrorKey] as? URLError, underlyingError.code == .timedOut {
-                    self.handleRequestTimeout()
-                } else {
-                    self.handleRequestError(error: error)
-                }
-                
-                completion(.failure(error))
-            }
-        }
-    }
-    
     /// 异步请求
     /// - Parameters:
     ///   - target: 请求接口
